abb41eed947fd23cb58169f3defb278a453aa240
beamedina <alexandra.medina@estudante.ifro.edu.br>
2024-11-01 14:25:21 -0400

    up

--- frontend II/atividadeOnclicked.js
+++ frontend II/atividadeOnclicked.js
@@ -1,9 +1,14 @@
 const miniaturas = document.querySelectorAll('.miniatura')
-const imgAmpliada = document.querySelector('.imgAmpliada')
+const imgAmpliada = document.querySelector('#imgAmpliada')
 
+console.log(miniaturas[0])
 miniaturas.forEach(miniatura => {
-    miniatura.addeventListener('click', () =>{
-        const src = miniatura.src;
-        console.log(src)
-    })
-})
+    miniatura.addEventListener('click', () => {
+        miniaturas.forEach(miniatura => {
+            miniatura.style.opacity = '0.4';
+        })
+        miniatura.style.opacity = '1';
+        imgAmpliada.src = miniatura.src;
+        imgAmpliada.style.display = 'block';
+    });
+});
