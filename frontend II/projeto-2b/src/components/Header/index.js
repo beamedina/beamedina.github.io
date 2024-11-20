4fff58416e759a7dae38799064ea9e8d4c556c64
beamedina <alexandra.medina@estudante.ifro.edu.br>
2024-11-20 14:27:57 -0400

    up

--- frontend II/projeto-2b/src/components/Header/index.js
+++ frontend II/projeto-2b/src/components/Header/index.js
@@ -1,24 +1,48 @@
-import Link from "next/link"
-import styles from "./Header.module.css"
+"use client";
 
+import { useState } from "react";
+import Link from "next/link";
+import styles from "./Header.module.css";
 
-export default function Header () {
-    return (
-        <header className={styles.header}>
-            <nav className={styles.nav} >
-                <ul className={styles.ul}>
-                    <li className={styles.li}>
-                       <Link className ={styles.linkzin} href='/'>Home</Link> 
-                    </li>
-                    <li className={styles.li}>
-                        <Link className ={styles.linkzin} href='/paris'>A Paris</Link>
-                    </li>
-                    <li className={styles.li}>
-                        <Link className ={styles.linkzin} href='/sobre'>Sobre</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <h1>Music</h1>
+      </div>
+      <button
+        className={styles.menuButton + (menuOpen ? " " + styles.open : "")}
+        onClick={toggleMenu}
+      >
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+      </button>
+      <nav className={styles.nav + (menuOpen ? " " + styles.navOpen : "")}>
+        <ul className={styles.ul}>
+          <li className={styles.li}>
+            <Link className={styles.link} href="/">
+              Home
+            </Link>
+          </li>
+          <li className={styles.li}>
+            <Link className={styles.link} href="/paris">
+              A Paris
+            </Link>
+          </li>
+          <li className={styles.li}>
+            <Link className={styles.link} href="/sobre">
+              Sobre
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
-
